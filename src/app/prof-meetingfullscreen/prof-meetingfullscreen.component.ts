266087dd9e827d1a2ae65877cbf71c4c53bfb5dd
Ralph Martin Flores <201811044@gordoncollege.edu.ph>
2021-09-29 23:49:08 +0800

    Fullmeeting screen now adjusts to viewport

--- src/app/prof-meetingfullscreen/prof-meetingfullscreen.component.ts
+++ src/app/prof-meetingfullscreen/prof-meetingfullscreen.component.ts
@@ -18,17 +18,15 @@ export class ProfMeetingfullscreenComponent implements OnInit{
 
   constructor(private observer: BreakpointObserver) { }
 
-  // ngOnInit(): void {
-  //   this.Dish();
-  //   window.onresize = this.Dish;
-  // }
-
   ngOnInit() {
-    this.Dish(); // calling the function of dish
-    window.onresize = this.Dish; // used window.onresize method = if the window has been changed by it size, the dish must be functioning..
-    // new Area();
-    // new Dish();
-    // new setWidth();
+    // Call dish function everytime window resizes
+    window.addEventListener("resize", () => {
+      this.Dish();
+    });
+    
+    // Run dish function at first page load
+    this.Dish();
+
     // Observes for breakpoint changes and changes sidenav mode to be more responsive
     this.observer
       .observe(['(max-width: 800px)'])
@@ -37,87 +35,13 @@ export class ProfMeetingfullscreenComponent implements OnInit{
         if (res.matches) {
           this.sidenav.mode = 'over';
           this.sidenav.close();
-          this.Dish();
-          window.onresize = this.Dish;
         } else {
           this.sidenav.mode = 'side';
           this.sidenav.open();
-          this.Dish();
-          window.onresize = this.Dish;
         }
       });
-      this.layout();
   }
 
-    //For layout of every video tiles
-    recalculateLayout() {
-    const gallery = document.getElementById("gallery");
-    const aspectRatio = 16 / 9;
-    const screenWidth = document.body.getBoundingClientRect().width;
-    const screenHeight = document.body.getBoundingClientRect().height;
-    const videoCount = document.getElementsByTagName("video").length;
-  
-    // npm install rect-scaler
-    function calculateLayout(
-      containerWidth: number,
-      containerHeight: number,
-      videoCount: number,
-      aspectRatio: number
-    ): { width: number; height: number; cols: number } {
-      let bestLayout = {
-        area: 0,
-        cols: 0,
-        rows: 0,
-        width: 0,
-        height: 0
-      };
-  
-      // brute-force search layout where video occupy the largest area of the container
-      for (let cols = 1; cols <= videoCount; cols++) {
-        const rows = Math.ceil(videoCount / cols);
-        const hScale = containerWidth / (cols * aspectRatio);
-        const vScale = containerHeight / rows;
-        let width;
-        let height;
-        if (hScale <= vScale) {
-          width = Math.floor(containerWidth / cols);
-          height = Math.floor(width / aspectRatio);
-        } else {
-          height = Math.floor(containerHeight / rows);
-          width = Math.floor(height * aspectRatio);
-        }
-        const area = width * height;
-        if (area > bestLayout.area) {
-          bestLayout = {
-            area,
-            width,
-            height,
-            rows,
-            cols
-          };
-        }
-      }
-      return bestLayout;
-    }
-  
-    const { width, height, cols } = calculateLayout(
-      screenWidth,
-      screenHeight,
-      videoCount,
-      aspectRatio
-    );
-  
-    gallery?.style.setProperty("--width", width + "px");
-    gallery?.style.setProperty("--height", height + "px");
-    gallery?.style.setProperty("--cols", cols + "");
-
-  }
-  
-  layout(){
-    let calcLayout= debounce(this.recalculateLayout, 50);
-    window.addEventListener("resize", calcLayout);
-    calcLayout();
-  }
   
 
   //Script JS
